b78c2a0a7ee42c4884d729202fdc059dc286566a
Cait Phillips <caitkp@caitkp-macbookair.roam.corp.google.com>
2015-06-21 19:56:48 -0400

    cleanup

--- chrome-extensions-tutorial/highlighter/background.js
+++ chrome-extensions-tutorial/highlighter/background.js
@@ -1,6 +1,9 @@
 
+// Variable to store the current search term value.
 var search_term;
 
+// Update the search term and tell all active tabs in the current window that
+// it had been updated.
 function updateSearchTerm(new_term) {
   console.log("updating to:" + new_term);
   search_term = new_term;
@@ -16,7 +19,10 @@ function updateSearchTerm(new_term) {
   });
 };
 
-
+// Dispatcher function to handle incoming requests. The background page needs to
+// listen to both the content script (which may request the search term when it
+// loads into a new tab), and the browser action, which may notify that the
+// search term has been updated.
 function handle_request(request, sender, sendResponse) {
   if (request.action == 'get_search_term') {
     console.log("sending search term...");
